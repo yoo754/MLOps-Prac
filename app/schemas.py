0f6b5b7760e2fa610df56363f73a08d18132b4f2
Hyejin Jung <yoo754@naver.com>
2025-05-27 15:27:04 +0900

    github action 추가전 정리

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
 
-# 임시 더미 클래스들
-class Wine(BaseModel):
-    pass
+# # 임시 더미 클래스들
+# class Wine(BaseModel):
+#     pass
 
-class Rating(BaseModel):
-    pass
+# class Rating(BaseModel):
+#     pass
 
-feature_names = []
+# feature_names = []
